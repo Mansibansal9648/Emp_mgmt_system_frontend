d6621209c883152d636fb8468f88f46c5b49d9db
jasvinderkhera <ankitkhera15@gmail.com>
2024-06-02 17:20:23 +0530

    signin page added, bootstrap and fontawesome added in index.html file

--- src/components/signin/signin.jsx
+++ src/components/signin/signin.jsx
@@ -1,6 +1,52 @@
+import React from "react"
+import './signin.css'
+
 function Signin(){
     return(
-        <h1>signin page</h1>
+        <div className="main_contain">
+        <div className="container">
+          <form action="">
+            <div className="text-center title_box">
+              <span className="pre_title"></span>
+              <h1 className="title">Dev Sages CRM</h1>
+            </div>
+            <h4 className="text-center">SIGN IN</h4>
+            <p className="text-center text-secondary">
+              Enter your credentials to access your account
+            </p>
+            <div className="mb-3">
+              <label htmlFor="email" className="mb-2 input_label">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="mb-2 input_label">
+                Password
+              </label>
+              <input
+                type="password"
+                name="text"
+                className="form-control"
+                placeholder="Enter your password"
+              />
+            </div>
+            <button className=" signin mb-3">SIGN IN</button>
+            <div className="text-center">
+              {" "}
+              <span>Forgot your password? </span>
+              <a href="" className="anchor">
+                Reset Password
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     )
 }
 export default Signin
