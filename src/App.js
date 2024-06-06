b04b0fec0331d95b2900594f8eb472b25db960cc
jasvinderkhera <ankitkhera15@gmail.com>
2024-06-06 22:08:02 +0530

    create employee form added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./components/signin/signin";
 import Dashboard from "./components/dashboard";
+import CreateUser from "./components/createuser/createuser";
+import Layout from "./components/common/layout/layout";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
+    
     <BrowserRouter>
+    <ToastContainer
+position="top-right"
+autoClose={3000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="colored"
+/>
       <Routes>
         <Route path="/" element={<Signin />}></Route>
         <Route path="/signin" element={<Signin />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
+
+        <Route path="/createuser" element={
+          <Layout>
+            <CreateUser />
+          </Layout>
+        }></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/common/layout/layout.jsx
+++ src/components/common/layout/layout.jsx
@@ -7,6 +7,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import "../common.css";
+import CreateUser from "../../createuser/createuser";
 
 function Layout(props) {
   return (
@@ -14,7 +15,7 @@ function Layout(props) {
       <div className="container-fluid">
         <div className="row">
           {/* Sidebar */}
-          <div className="col-md-2 sidebar ">
+          <div className="col-md-2 sidebar border border-1 ">
             <div class="sidebar-profile  text-center ">
               <div className="mt-3 ">
                 <span className="logo mx-4 ">DevSages CRM</span>
@@ -28,14 +29,14 @@ function Layout(props) {
             </div>
             <div className="container d-flex justify-content-center align-items-center mt-5">
               <ul className="list-unstyled">
-                <li className="mx-2 mt-3 w-100 sidebar-menu p-2 border rounded">
-                  <Link to="/dashboard">
-                    <DashboardIcon className="mx-2" />
+                <li className="mx-2 mt-3 w-100 sidebar-menu p-2 border rounded ">
+                  <Link to="/dashboard" className="nav-link">
+                    <DashboardIcon className="mx-2"/>
                     Dashboard
                   </Link>
                 </li>
                 <li className="mx-2 mt-3 w-100 sidebar-menu p-2 border rounded">
-                  <Link to="/employees">
+                  <Link to="/employees" className="nav-link">
                     <BadgeIcon className="mx-2" />
                     Employees
                   </Link>
@@ -62,8 +63,8 @@ function Layout(props) {
             </div>
           </div>
           {/* Navbar */}
-          <div className="col-md-10 p-0 border-0">
-            <div className="container-fluid navbar d-flex justify-content-space-between ">
+          <div className="col-md-10 p-0">
+            <div className="container-fluid navbar d-flex justify-content-space-between  border border-1 ">
               <button className="rounded-circle">
                 <MenuOpenIcon />
               </button>
